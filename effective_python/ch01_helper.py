057c9212ce0f579bf03a035ae473916102ed46d8
diligejy <sjy049@gmail.com>
2020-06-22 01:19:56 +0900

    helper function

--- effective_python/ch01_helper.py
+++ effective_python/ch01_helper.py
@@ -15,3 +15,27 @@ opacity = my_values.get('opacity', [''])[0] or 0
 print('Red :          %r' % red)
 print('Green :        %r' % green)
 print('Opacity :      %r' % opacity)
+
+# 삼항 표현식으로 조금 더 명확하게 표현
+red = my_values.get('red', [''])
+red = int(red[0]) if red[0] else 0
+
+# 조금 더 명확히
+green = my_values.get('green', [''])
+if green[0]:
+    green = int(green[0])
+else:
+    green = 0
+
+
+# 근데 이게 반복되면? 귀찮잖아. -> 헬퍼함수로 바꾸자.
+def get_first_int(values, key, default = 0):
+    found = values.get(key, [''])
+    if found[0]:
+        found = int(found[0])
+    else:
+        found = default
+    return found
+
+# 함수로 바꾸니까 매번 안해도 되고 명확히 보여주네
+green = get_first_int(my_values, 'green')
