72a33516073de57b976e1b54bbffece749c1a68b
diligejy <sjy049@gmail.com>
2022-05-29 23:27:07 +0900

    unittest part 1

---
+++ python_unit_test/pay/tests/test_line_item.py
@@ -0,0 +1,12 @@
+from sympy import Line
+from pay.order import LineItem
+
+
+def test_line_item_default() -> None:
+    line_item = LineItem("Test", 100)
+    assert line_item.total == 100
+
+
+def test_line_item() -> None:
+    line_item = LineItem("Test", 200, 5)
+    assert line_item.total == 1000
---
+++ python_unit_test/pay/tests/test_order.py
@@ -0,0 +1,26 @@
+from pay.order import LineItem, Order
+from pay.order import OrderStatus
+
+
+def test_empty_order_total() -> None:
+    order = Order()
+    assert order.total == 0
+
+
+def test_order_total() -> None:
+    order = Order()
+    order.line_items.append(LineItem(name="Test", price=100))
+    assert order.total == 100
+
+
+def test_order_multi_total() -> None:
+    order = Order()
+    order.line_items.append(LineItem(name="Test", price=100))
+    order.line_items.append(LineItem(name="Test", price=100))
+    assert order.total == 200
+
+
+def test_order_pay() -> None:
+    order = Order()
+    order.pay()
+    assert order.status == OrderStatus.PAID
