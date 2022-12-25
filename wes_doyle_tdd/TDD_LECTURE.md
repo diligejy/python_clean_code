5e4c536ce5982b2161b75641c3a7af02bc0b8c51
diligejy <sjy049@gmail.com>
2022-12-25 23:43:38 +0900

    add: tdd basic ing

---
+++ wes_doyle_tdd/TDD_LECTURE.md
@@ -0,0 +1,5 @@
+## 3 Rules of TDD
+
+1. Write a Failing Test First
+2. Write Just Enough test to Fail
+3. Write Just Enough Code to Pass
---
+++ wes_doyle_tdd/test/test_ner_client.py
@@ -0,0 +1,25 @@
+import unittest 
+from ner_client import NamedEntityClient
+from test_double import NerModelTestDouble
+
+class TestNerClient(unittest.TestCase):
+    
+    # { ents : [{...}],
+    #   html : "<span>..."}
+    
+    def test_get_ents_returns_dictionary_given_empty_string_causes_empty_spacy_doc_ents(self):
+        model = NerModelTestDouble("eng")
+        model.returns_doc_ents([])
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("")
+        self.assertIsInstance(ents, dict)
+    
+    def test_get_ents_returns_dictionary_given_nonempty_string_causes_empty_spacy_doc_ents(self):
+        model = NerModelTestDouble("eng")
+        model.returns_doc_ents([])
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("Madison is a city in Wisconsin")
+        self.assertIsInstance(ents, dict)
+        
+    
+    
